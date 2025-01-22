b4882f1ea4f52a72fe8b966811c72ce068800558
Mohammad9194 <mohmmadshaikh004@gmail.com>
2025-01-22 11:56:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,39 +1,27 @@
-// Abstract class
-abstract class Shape {
-    // Abstract method (no body)
-    abstract void draw();
-
-    // Concrete method (has body)
-    void showShapeType() {
-        System.out.println("This is a shape.");
-    }
+abstract class Vehicle {
+    abstract void speed();
 }
 
-// Subclass that extends the abstract class
-class Circle extends Shape {
+interface Serviceable {
+    void service();
+}
+
+class Car extends Vehicle implements Serviceable {
     @Override
-    void draw() {
-        System.out.println("Drawing a circle.");
+    void speed() {
+        System.out.println("Car speed: 120 km/h");
     }
-}
 
-// Subclass that extends the abstract class
-class Rectangle extends Shape {
     @Override
-    void draw() {
-        System.out.println("Drawing a rectangle.");
+    public void service() {
+        System.out.println("Car is being serviced.");
     }
 }
 
-// Main class
 public class Main {
     public static void main(String[] args) {
-        Shape circle = new Circle();
-        circle.showShapeType();
-        circle.draw();
-
-        Shape rectangle = new Rectangle();
-        rectangle.showShapeType();
-        rectangle.draw();
+        Car car = new Car();
+        car.service();
+        car.speed();
     }
 }
